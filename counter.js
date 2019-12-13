5ba574a9fb113e180a8752997aaf46cf9986e200
aozorabunko <git@aozora.gr.jp>
2019-12-14 07:00:38 +0900

    2019-12-14

--- counter.js
+++ counter.js
@@ -1,5 +1,6 @@
-var myhost = "www.aozora.gr.jp";
+var myhost = location.hostname;
+var mirror = "mirror.aozora.ne.jp";
 var main = "www.aozora.gr.jp";
 if (myhost == main) {
-document.write("<br><img src=../../cgi/npc.cgi?L=http://www.aozora.gr.jp/>");
+document.write("<br><img src=http://www.homesite.solution.ne.jp/cgi-bin/npc.cgi?T=0&C=0000FF,AAFFAA&DI=2&W=8&L=http://www.aozora.gr.jp/>");
 }
